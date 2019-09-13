4d9aca43d088ded6bf3a4d058c87e408c6370998
hnbeck <beckhn@gmail.com>
2019-09-13 20:03:30 +0200

    all but pengine.ask is in Tau now

--- web/webProlog.pl
+++ web/webProlog.pl
@@ -8,32 +8,61 @@
 
 	:- use_module(library(dom)).
 	:- use_module(library(js)).
-	:- use_module(library(lists)).
+	:- use_module(library(lists)). 
 
+	init :-
+		get_by_id('btplay', Play),
+		get_by_id('btstop', Stop),
+		bind(Play, click, _, playAction),
+		bind(Stop, click, _, stopAction),
+		write('Binding done').
+
+	playAction :-
+		nextQueryArgs(PA, PP),
+		Term =.. [playCard, PA, PP, 'PA2', 'PP2', 'Flag', 'Msg'],
+		msg2JS('gameturn', Term).
+		
+	stopAction :-
+		get_by_id('btplay', Play),
+		set_attr(Play, disabled, true), 
+		set_style(Play, 'background-color', 'white'),
+		nextQueryArgs(PA, PP),
+		Term =.. [stop, PA, PP, 'Flag', 'Msg'],
+		msg2JS('gamestop', Term).
+
+	msg2JS(FktID, Term) :-
+		writeHTML('Tauhtml',Term, String),
+		prop(FktID, JSFkt),
+		apply(JSFkt, [], _).
+
+	nextQueryArgs(PA2, PP2) :-
+		fact(pa2, PA),
+		fact(pp2, PP),
+		nextPlayer(PA, PP, PA2, PP2).
 
 	% +Term : a Tau Prolog term
 	% -HMTLString : the term as String
 	writeHTML(ID, Term, HTMLString) :-
 		get_by_id(ID, HTML),
 		open(HTML, write, Stream), 
-		write(Stream, Term),
+		write(Stream, Term), % for Debug
 		close(Stream),
 		get_html(HTML, HTMLString).
 
 	% -Term : a Tau Prolog Term
-	readHTML(Term) :-
-		get_by_id('TauInput', HTML),
+	readHTML(ID, Term) :-
+		get_by_id(ID, HTML),
 		open(HTML, read, Stream), 
 		read(Stream, Term),
-		clode(Stream).
+		close(Stream).
 
-	nextPlayer(player(1, F1), player(2, F2), player(2, F2), player(1, F1)).
+	nextPlayer(PA, PP, PP, PA).
 	% +JSObjectID: an reference to a JS object containing the answer of a Pengine query
 	% -TauTerm: the Pengine answer as Tau Prolog Term
-	analyse(JSObject, TauTerm) :-
-		parseTerm(JSObject, TauTerm),
-		fact(msg, Msg),
-		writeHTML('pout', Msg, String).
+	%analyse(JSObject, TauTerm) :-
+	%	parseTerm(JSObject, TauTerm),
+	%	fact(msg, Msg),
+	%	writeHTML('pout', Msg, String).
 
 	% if element is not defined
 	parseTerm(Elem, _) :- var(Elem).
@@ -64,6 +93,7 @@
 	% the answer will be included in Tau database as
 	% fact(p1, AnswerTerm)
 	holdTerm(TauTerm, H) :-
+		% write(fact(H, TauTerm)), % for debug
 		retractall(fact(H, _)),
 		asserta(fact(H, TauTerm)).
 
