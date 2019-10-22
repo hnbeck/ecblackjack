8b459c8c501fb0fbc4f87cfe3d24ae854f1be09b
hnbeck <beckhn@gmail.com>
2019-10-22 14:04:29 +0200

    different style of tau js interaction

--- ecBlackJack.pl
+++ ecBlackJack.pl
@@ -1,32 +1,162 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% Simple example for Prolog game, including Tau-Prolog and Pengine
+% Simple example for a Black Jack Prolog game, including Tau-Prolog and Pengine
 %
-% Based on a example for an article for the German magazine Heise Online
+% Based on an example for an article for the German magazine Heise Online
+% written by me.
 % Code uses asserta to store the card deck. 
 % 
 % Author: Hans N. Beck (c)
-% Last Change: 28.08.2019
+% Last Change: 05.10.2019
 %
 % License: MIT 
 %
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-:- module(ecBlackJack, [playGame/3, stop/4, showDeck/1, playCard/7]).
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+:- module(ecBlackJack, [playGame/3, stop/4, listDeck/2, playCard/5]).
+
 :- use_module(library(pengines)).
 :- use_module(library(sandbox)).
 
 :- dynamic card/3.
-
 :- multifile sandbox:safe_primitive/1.
 
-% card deck
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Note: this is the server side Prolog level of the application. It implements
+% the game rules, the objects of the games (the cards), their logical structure
+% It declares which structural changes are associated with the playing actions. 
+% But it says nothing about where the cards laying, if we have a desk or 
+% using only our hands. It says nothing about the order of the cards or time. 
+% That all is not a natural part of the game itself.
+% Display, geometry and such things are responsibility of the browser, which is
+% the Javascript or Tau-Prolog level
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% picture deck cards
+% card structure is: card(card color, card name, points)
+% card color is one of herz caro kreuz schippe (German identifier)
 card(herz, 10, 10).
 card(herz, bube, 11).
 card(herz, dame, 12).
 card(herz, koenig, 13).
 card(herz, ass, 14).
+card(karo, 10, 10).
+card(karo, bube, 11).
+card(karo, dame, 12).
+card(karo, koenig, 13).
+card(karo, ass, 14).
+card(kreuz, 10, 10).
+card(kreuz, bube, 11).
+card(kreuz, dame, 12).
+card(kreuz, koenig, 13).
+card(kreuz, ass, 14).
+card(schippe, 10, 10).
+card(schippe, bube, 11).
+card(schippe, dame, 12).
+card(schippe, koenig, 13).
+card(schippe, ass, 14).
 
-% who wins ?
+% Provide access to card structure
+% cardPoints(+ Card Color, +Name, -Points)
+cardPoints(Farbe, Name, Points) :-
+	card(Farbe, Name, Points).
+
+%%%%%%%%%%%%%%%%%%%%%%% Fill up card data base %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% initialize deck. Picture cards were added by hand -> see above
+% now fill up the number cards
+% initDeck(-list of all known cards)
+initDeck() :-
+	fillDeck(herz, 2, 9),	
+	fillDeck(karo, 2, 9),	
+	fillDeck(schippe, 2, 9),
+	fillDeck(kreuz, 2, 9).
+
+% add a card to Prolog db = card deck here
+% stop
+addFact(_, End, End).
+% addFact(+color, end number, -index)
+addFact(Farbe, End, I) :-
+	asserta(card(Farbe, I, I)),
+	I2 is I + 1,
+	addFact(Farbe, End, I2).
+
+% fill all cards between start and end number
+% fillDeck(+color, +start number, +end Number)
+fillDeck(Farbe, Start, End) :-
+	End2 is End + 1, 
+	addFact(Farbe, End2, Start).
+
+% listDeck(-List: list of all known cards, -N: number of cards )
+listDeck(List, N) :- 
+	findall(card(A, B, C), card(A, B, C), List),
+	length(List, N).
+
+% needed for pengine - declare safe predicate
+sandbox:safe_primitive(ecBlackJack:listDeck(_,_)).
+
+% newPlayer(+player number, -Player structure)
+% player structure is player(Playernumber, List of his cards)
+% List of cards is also called "Playfield" or Field for short
+newPlayer(Num, player(Num, [])).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% play actions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% idea: there is player 1 and player 2. In every turn, one is the active player
+% after a play action, the active and passive player changes
+% Init the game
+% playGame(- P1: first player, -P2: scnd player, - Msg: a message string)
+playGame(P1, P2, Msg) :-
+	initDeck(), 
+	newPlayer(1, P1), 
+	newPlayer(2, P2),
+	format(atom(Msg), '~s', ["Game can start now"]).
+
+% Kommando: game over, can be triggerd by player or system
+% stop(+P1: a player, -P2: changed player, -Flag: stop or continue, -Msg: Message)
+% if player 1 was active player
+stop(player(1, Feld1), player(2, Feld2), stop, Msg) :-
+	stateWinner(Feld1, Feld2, Msg).
+% if active player was player 2
+stop(player(2, Feld2), player(1, Feld1), stop, Msg) :-
+	stateWinner(Feld1, Feld2, Msg).
+
+% playCard(+player, -updated player)
+playCard(player(Num, Field), player(Num, Field2), Card, Flag, Msg) :-
+	drawCard(Card),
+	append(Field, [Card], Field2),
+	stopCondition(Field2, Flag), 
+	format(atom(Msg), "your draw ~w\n",  [Card]).
+% drawCard was false - no Cards anymore in Deck - stop Game
+playCard(P1, P2, _, over, Msg) :-
+	stop(P1, P2, _, Msg ).
+
+%%%%%%%%%%%%%%%%%%%% transformations - not visible outside %%%%%%%%%%%%%%%%%%%%%
+
+% Draw a Card from deck. The card is removed from deck, which means here
+% is is removed from the Prolog database
+% drawCard(-card structure)
+% can be false if deck is empty
+drawCard(card(A, B, C)) :-
+	listDeck(CardList, N),
+	random_between(1, N, Num), 
+	nth1(Num, CardList, card(A, B, C)),
+	retractall(card(A,B,C)). 
+
+sandbox:safe_primitive(ecBlackJack:drawCard(_)).
+
+% +Feld1 cards of player 1 
+% +Feld2 cards of player 2
+% -Winner hold the number of winning player
+stateWinner(Feld1, Feld2, Msg) :-
+	winner(Feld1, Feld2, Winner), 
+	format(atom(Msg), "The Winner is ~d\n", [Winner]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% game and winning rules %%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% which card  wins over which other?
 win(dame, bube).
 win(koenig, dame).
 win(ass, koenig).
@@ -36,195 +166,67 @@ winAlso(X,Y) :-
 	win(X,dame),
 	win(dame, Y).
 
-%%%%%%%%% Expand data base %%%%%%%%%%%%%%%%
-
-% stop
-addFact(_, End, End).
-% addFact(+color, end number, -index)
-addFact(Farbe, End, I) :-
-	asserta(card(Farbe, I, I)),
-	I2 is I + 1,
-	addFact(Farbe, End, I2).
-
-
-
-% fill all cards between start and end
-% fillDeck(+color, +start number, +EndNummer)
-fillDeck(Farbe, Start, End) :-
-	End2 is End + 1, 
-	addFact(Farbe, End2, Start).
-
-% initialize deck
-% initDeck(-list of all known cards)
-initDeck(List) :-
-	fillDeck(herz, 2, 9),	
-	showDeck(List).
-
-% showDeck(-list of all known cards)
-showDeck(List) :- 
-	findall(X, card(_, X, _), List).
-
-sandbox:safe_primitive(ecBlackJack:showDeck(_)).
-
-% Example for a data structure
-% newPlayer(+player number, -Player structure)
-newPlayer(Num, player(Num, [])).
-
-%%%%%%%%%%% Alternative for deck - using arrays and not the database %%%%%%%%%%%%%%%
-
-addCard2(Deck, Farbe, Name, Deck2) :-
-	card(Farbe, Name, X),
-	append(Deck, [card(Farbe, Name, X)], Deck2).
-% variant
-addCard3(Deck, Farbe, Name, Deck2) :-
-	cardBuilder(Farbe, Name, C),
-	append(Deck, [C], Deck2).
-% generator
-cardBuilder(Farbe, Name, card(Farbe, Name, X)) :-
-	card(Farbe, Name, X).
-
-
-%%%%%%%%%%%%%%%%%%% play actions %%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-% random draw
-% drawCard(-structure)
-drawCard(card(A, B, Num)) :-
-	random_between(2,14, Num), 
-	card(A, B, Num),
-	retractall(card(A,B,Num)). 
-
-sandbox:safe_primitive(ecBlackJack:drawCard(_)).
-
-% cardPoints(+color, +Name, -Points)
-cardPoints(Farbe, Name, Points) :-
-	card(Farbe, Name, Points).
-
-
-%%%% play card
-
-% playCard(+player, -updated player)
-playCard(player(Num, Field), player(Num, Field2), Card, Msg) :-
-	drawCard(Card), 
-	append(Field, [Card], Field2), 
-	format(atom(Msg), "your draw ~w\n",  [Card]).
-
-%% command for Interpreter
-playCard(PA1, PP, PA2, PP,  go, Card, Msg) :-
-	playCard(PA1, PA2, Card, Msg).
-
-
-%%%%%%%%%%%%%%%%%%%%% winning rules %%%%%%%%%%%%%%%%%%
-
-%% Evaluation of cards
-
-% sumPoints(+card, +sum before, -sum plus card points)
-sumPoints(card(_,_,Point), Sum, Sum2) :-
-	Sum2 is Sum + Point.
-
-% Abbruchbedingung
-sumCards([], Sum, Sum).
-% sumCards(+card list, +points before, -summized points)
-sumCards([C|Cards], Sum, Sum3) :-
-	sumPoints(C, Sum, Sum2),
-	sumCards(Cards, Sum2, Sum3).
-% sumCards(s(+card list, -sum of all card points)
-sumCards(Cards, Sum) :-
-	sumCards(Cards, 0, Sum).
-
-
-%%% winning rules
-
 % test if 21 is crossed
 % distance is the distance to 21 from both directions
 
 % cardsTest(+card list, -distance)
 cardsTest(Cards, Distance) :-
-	sumCards(Cards, Sum), 
+	cardsSum(Cards, Sum), 
 	Distance is 21 - Sum.
-% test < 21
-cardsOk(Distance, ok) :-
+% test of < 21
+cardsOk(Distance, go) :-
 	Distance >= 0.
-% Test over 21
-cardsOk(Distance, loser) :-
+% Test of > 21
+cardsOk(Distance, stop) :-
 	Distance < 0.
 
-%%% Rules
+%%% sum the points of all cards of given list
+% cardsSum(s(+card list, -sum of all card points)
+cardsSum(Cards, Sum) :-
+	cardsSum(Cards, 0, Sum).
+% stop condition
+cardsSum([], Sum, Sum).
+% cardsSum(+card list, +points before, -summized points)
+cardsSum([C|Cards], Sum, Sum3) :-
+	sumPoints(C, Sum, Sum2),
+	cardsSum(Cards, Sum2, Sum3).
 
-winner( [], [], 0). 
+% sum a card to the current points
+% sumPoints(+card, +sum before, -sum plus card points)
+sumPoints(card(_,_,Point), Sum, Sum2) :-
+	Sum2 is Sum + Point.
 
-winner([C|Cs1], [C2|Cs2], Winner) :-
+%%%%%%%%%%%%%%%%% winning rules %%%%%%%%%%%%%%%%%%%
+
+% check if game is over - this is when a player is over 21 points
+stopCondition( [], go).
+% stopCondition(+L:list of cards, -Flag: go or stop)
+stopCondition([C|Cs1], Flag) :-
 	cardsTest([C|Cs1], Distance1), 
-	cardsTest([C2|Cs2], Distance2),
-	winner(Distance1, Distance2, Winner).
+	cardsOk(Distance1, Flag).
 
 % Evaluation of distance to 21 for every player and determing winner
 % player 1 wins if <21 and player 2 not
-winner(Distance1, Distance2, 1) :-
-	cardsOk(Distance1, ok),
-	cardsOk(Distance2, loser). 
+winner( [], [], 0). 
+%
+winner([C|Cs1], [C2|Cs2], Winner) :-
+	cardsTest([C|Cs1], Distance1), 
+	cardsTest([C2|Cs2], Distance2),
+	judgement(Distance1, Distance2, Winner).
+
+judgement(Distance1, Distance2, 1) :-
+	cardsOk(Distance1, go),
+	cardsOk(Distance2, stop). 
 % player 2 wins if <21 and player1 not	
-winner(Distance1, Distance2, 2) :- 
-	cardsOk(Distance2, ok),
-	cardsOk(Distance1, loser). 
+judgement(Distance1, Distance2, 2) :- 
+	cardsOk(Distance2, go),
+	cardsOk(Distance1, stop). 
 % player 1 wins if closer to 21
-winner(Distance1, Distance2, 1) :-
+judgement(Distance1, Distance2, 1) :-
 	Distance1 < Distance2.
 % player 2 wins if closer to 21
-winner(Distance1, Distance2, 2) :-
+judgement(Distance1, Distance2, 2) :-
 	Distance1 > Distance2.
 
 % no winner in all ather cases
-winner(_, _, 0).
-
-
-%%%%%%%%%%%%% simpler Interpreter for the fame %%%%%%%%%%%%%
-%
-% Init the game
-playGame(P1, P2, Msg) :-
-	initDeck(_), 
-	newPlayer(1, P1), 
-	newPlayer(2, P2),
-	format(atom(Msg), '~s', ["Game can start now"]).
-
-
-% Kommando: game over
-stop(player(1, Feld1), player(2, Feld2), stop, Msg) :-
-	stateWinner(Feld1, Feld2, Msg).
-% if active player was player 2
-stop(player(2, Feld2), player(1, Feld1), stop, Msg) :-
-	stateWinner(Feld1, Feld2, Msg).
-
-% +Feld1 cards of player 1 
-% +Feld2 cards of player 2
-% -Winner hold the number of winning player
-stateWinner(Feld1, Feld2, Msg) :-
-	winner(Feld1, Feld2, Winner), 
-	format(atom(Msg), "The Winner is ~d\n", [Winner]).
-
-
-% nur player 1 
-% +A active player +P passive player -Finish finish flag
-%do(stop, A, P, A, P, Finish, Msg) :- 
-%	call(stop, A, P, Finish, Msg).
-
-% draw card and play
-%do(playCard, A, P, A2, P, Finish, Msg) :-
-%	call(playCard, A, A2, Finish, Msg).
-
-%do(showDeck, A, P, A, P, go, Msg) :-
-%	showDeck(L), 
-%	format(atom(Msg), 'Current Deck: ~p\n', [L]).
-
-% wrong command
-%do(_, A, P, A, P, _, Msg) :-
-%	format(atom(Msg), 'Rubbisch, commands are <playCard> or <stop> \n').
-
-% the do command is redunant by using with pengines
-% showDeck, playCard and stop will be called directly
-
-%% this is also done in Tau Prolog now
-%nextPlayer(player(1, F1), player(2, F2), player(2, F2), player(1, F1)).
-%nextPlayer(player(2, F2), player(1, F1), player(1, F1), player(2, F2)).
-
-% Call
+judgement(_, _, 0).
--- web/webProlog.pl
+++ web/webProlog.pl
@@ -1,47 +1,70 @@
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 % Tau Prolog code for the Prolog part in the browser
+% This is the bridge between the graphics in Processing and user and the
+% the game itself provided by the Prolog server (SWI Prolog via Pengines)
+% It takes player information from Pengine and triggers querys to Pengine
+% as response to user actions
 % 
-%  Autor: Hans N. Beck (c)
+% Author: Hans N. Beck (c)
+% Last Change: 05.10.2019
 %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% License: MIT 
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 :- use_module(library(dom)).
 :- use_module(library(js)).
-:- use_module(library(lists)). 
+:- use_module(library(lists)).  
 
+% setup DOM event bindings and other inits
 init :-
-	get_by_id('btplay', Play),
+	%get_by_id('btplay', Play),
 	get_by_id('btstop', Stop),
-	bind(Play, click, _, playAction),
+	%bind(Play, click, _, playAction),
 	bind(Stop, click, _, stopAction),
-	%holdTerm(card(herz, ass, 14), current),
-	write('Binding done').
+	holdTerm(nextPlayer(p2, p1), next),
+	write('Tau Prolog: Binding done').
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%  The action interface - every action triggers a query over pengine to
+% SWI Prolog in order to do a game move
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% user want to play a card - build the Prolog query to be sent over Pengine
+% The identifier between '' are unbound variables bind by Prolog
+% and the terms binded will be stored in Tau Prolog associated with this identifier
+% Example "Current" will be bound with the card(...) drawn and stored in 
+% Tau Prolog as state(current, card(...)
 playAction :-
-	nextQueryArgs(PA, PP),
-	Term =.. [playCard, PA, PP, 'PA2', 'PP2', 'Flag', 'Current', 'Msg'],
+	nextPlayerArg(P, PResultStr),
+	prop('deckCostume', DeckID),
+	prop(DeckID, 'mytext', Fkt),
+	apply(Fkt, ['WUMMER'], _),
+	Term =.. [playCard, P, PResultStr, 'Current', 'Flag', 'Msg'],
 	msg2JS('sendPengine', Term).
 	
+% user wants to stop the game
 stopAction :-
 	get_by_id('btplay', Play),
 	set_attr(Play, disabled, true), 
 	set_style(Play, 'background-color', 'white'),
-	nextQueryArgs(PA, PP),
-	Term =.. [stop, PA, PP, 'Flag', 'Msg'],
+	state(p1, P1),
+	state(p2, P2),
+	Term =.. [stop, P1, P2, 'Flag', 'Msg'],
 	msg2JS('sendPengine', Term).
 
+% write the term to be send via Pengine into a DOM node to get it as string
 msg2JS(FktID, Term) :-
 	writeHTML('Tauhtml',Term, String),
 	prop(FktID, JSFkt),
 	apply(JSFkt, [], _).
 
-nextQueryArgs(PA2, PP2) :-
-	state(pa2, PA),
-	state(pp2, PP),
-	nextPlayer(PA, PP, PA2, PP2).
-
+% write the query term in a non visible DOM element where it can be accessed
+% at JS Level
 % +Term : a Tau Prolog term
 % -HMTLString : the term as String
 writeHTML(ID, Term, HTMLString) :-
@@ -51,6 +74,7 @@ writeHTML(ID, Term, HTMLString) :-
 	close(Stream),
 	get_html(HTML, HTMLString).
 
+% read in from a DOM element
 % -Term : a Tau Prolog Term
 readHTML(ID, Term) :-
 	get_by_id(ID, HTML),
@@ -58,24 +82,101 @@ readHTML(ID, Term) :-
 	read(Stream, Term),
 	close(Stream).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Player control: at start the SWI Prolog application provides to playerStr
+% it is the task of the UI level to determine which player is the active player
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% exchange P1, P2
+nextPlayerArg(P, PStr) :-
+	state(next, nextPlayer(A, B)), 
+	holdTerm(nextPlayer(B, A), next),
+	state(A, P),
+	playerStr(A, PStr).
+	
+playerStr(p1, 'P1').
+playerStr(p2, 'P2').	
+
+% determine the active player
+activePlayer(P) :-
+	state(next, nextPlayer(Pid, _)),
+	state(Pid, P).
+
+% determine the active player
+activePlayerNo :-
+	activePlayer(P), 
+	playerNo(P, PNum),
+	atomic_list_concat( ['Player ', PNum], Text),
+	writeHTML('Tauhtml', Text, String).
+
+activePlayerNo :-
+	writeHTML('Tauhtml', '', String).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Bridge to the visual representation of things
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 costume(Farbe, Name) :-
-	state(pp2, P),
+	activePlayer(P),
 	playerNo(P, PNo),
 	atomic_list_concat([Farbe, Name], File), 
 	% write('Prolog costume File'), write(File), write(PNo),  %for debug
 	prop('newCostume', JSFkt), 
 	apply(JSFkt, [File, PNo], _).
 
-nextPlayer(PA, PP, PP, PA).
 
 playerNo(player(PNo, _), PNo).
 	
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%       Pengine  - Tau Prolog interface
+%
+%  The following predicates take the answer from a Pengine Query which is 
+% given as JS Object and transform it back to a Prolog statement.
+% This will be persistent via asserta
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% store the fact under reference of property
+% which means if pengine query contains variable P1
+% the answer will be included in Tau database as
+% state(p1, AnswerTerm)
+holdTerm(TauTerm, H) :-
+	% write('Taustate '), write(state(H, TauTerm)), % for debug
+	retractall(state(H, _)),
+	asserta(state(H, TauTerm)).
+
+% Pengine answer is coded as a JS object which is a list of properties
+% go through all properties given by the list and parse them
+% if all is parsed message is available and can put out
+% here, the SWI Prolog applicaton is designed to bind the Variable MSG
+% with a message to be displayed in use interface
+takeResult([], _, _) :- 
+	state(current, card(Farbe, Name, _)),
+	state(msg, Msg),
+	writeHTML('pout', Msg, String),
+	costume(Farbe, Name),!.
+% property list done, last action is to give back the message in msg
+takeResult([], _, _) :- 
+	state(msg, Msg),
+	writeHTML('pout', Msg, String).
+% takeResult(+Propertylist, +id of the variable containing the js object, -Tau Term)
+% H is one property which is identical to the name of a bound variable in Pengine answer!
+takeResult([H|T], JSObjectID, Term) :-
+	prop(JSObjectID, JSObject),
+	prop(JSObject, H, SubJSObject),
+	parseTerm(SubJSObject, TauTerm),
+	holdTerm(TauTerm, H),
+	takeResult(T, JSObjectID, Term).
+	
+
 % +JSObjectID: an reference to a JS object containing the answer of a Pengine query
+% every variable is a property containing an JS object for its bining
+% Example if Penge answer binds P to player(1,name) then there is something lile
+% {..."P":{functor:player, args:[1,name]}....}
 % -TauTerm: the Pengine answer as Tau Prolog Term
-%analyse(JSObject, TauTerm) :-
-%	parseTerm(JSObject, TauTerm),
-%	state(msg, Msg),
-%	writeHTML('pout', Msg, String).
 
 % if element is not defined
 parseTerm(Elem, _) :- var(Elem).
@@ -88,7 +189,6 @@ parseTerm(JSObject, TauTerm) :-
 	parseList(ArgList, TermList),
 	append([Functor], TermList, TermList2),
 	TauTerm =.. TermList2.
-
 % if elem is a list
 parseList([], []).
 parseList([Head | Tail ], [Head2 | Tail2]) :-
@@ -100,35 +200,4 @@ parseList([Head | Tail ], [Head2 | Tail2]) :-
 		)
 	),
 	parseList(Tail, Tail2).
-
-% store the fact under reference of property
-% which means if pengine query contains variable P1
-% the answer will be included in Tau database as
-% state(p1, AnswerTerm)
-holdTerm(TauTerm, H) :-
-	% write('Taustate '), write(state(H, TauTerm)), % for debug
-	retractall(state(H, _)),
-	asserta(state(H, TauTerm)).
-
-% go through all properties given by the list and parse them
-% if all is parsed message is available and can put out
-takeResult([], _, _) :- 
-	state(current, card(Farbe, Name, _)),
-	state(msg, Msg),
-	writeHTML('pout', Msg, String),
-	costume(Farbe, Name),!.
-	
-takeResult([], _, _) :- 
-	state(msg, Msg),
-	writeHTML('pout', Msg, String).
-	
-takeResult([H|T], JSObjectID, Term) :-
-	prop(JSObjectID, JSObject),
-	prop(JSObject, H, SubJSObject),
-	parseTerm(SubJSObject, TauTerm),
-	holdTerm(TauTerm, H),
-	takeResult(T, JSObjectID, Term).
-	
-
-
 
